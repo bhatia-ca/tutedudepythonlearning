6c2a4d45df7d08e99fdb9a2c70aeefc8d0db86ab
bhatia-ca <94182230+bhatia-ca@users.noreply.github.com>
2025-05-17 01:32:27 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,8 @@
+number=int(input('please provide a random nuber:    '))
+if (number % 2 == 0):
+    print(str(number) +" " + "is a even number")
+else:
+    print(str(number) +" " +'is a Odd number')
+
+
+
